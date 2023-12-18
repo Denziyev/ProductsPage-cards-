fefd18cf3e9eeb6f124b1b5f2e3313e905c1e11e
İlkin <ilkinhd@code.edu.az>
2023-12-18 22:32:19 +0400

    First commit

---
+++ products.js
@@ -0,0 +1,38 @@
+
+var watches=[
+    {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:2299000,
+        About:'aboutme',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'mainimage',
+        images:['image2','image3'],
+    },
+    {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:2299000,
+        About:'aboutme',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'mainimage',
+        images:['image2','image3'],
+    },
+    {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:2299000,
+        About:'aboutme',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'mainimage',
+        images:['image2','image3'],
+    }
+]
+
+console.log(watches)
