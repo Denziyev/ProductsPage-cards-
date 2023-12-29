e5e8f2443ee4f3fe4a47e1dd34f478d819262642
İlkin <ilkinhd@code.edu.az>
2023-12-29 14:20:35 +0400

    done

--- products.js
+++ products.js
@@ -4,35 +4,147 @@ var watches=[
         Brand:'OTTO',
         Title:'WEIMAR-40mm',
         Rate:2,
-        Price:2299000,
-        About:'aboutme',
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
         firstlink:'firstlinkk',
         secondlink:'secondlinkk',
-        mainimage:'mainimage',
-        images:['image2','image3'],
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
     },
     {
         Brand:'OTTO',
         Title:'WEIMAR-40mm',
-        Rate:2,
-        Price:2299000,
-        About:'aboutme',
+        Rate:1,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
         firstlink:'firstlinkk',
         secondlink:'secondlinkk',
-        mainimage:'mainimage',
-        images:['image2','image3'],
-    },
-    {
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:3,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:5,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
         Brand:'OTTO',
         Title:'WEIMAR-40mm',
         Rate:2,
-        Price:2299000,
-        About:'aboutme',
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
         firstlink:'firstlinkk',
         secondlink:'secondlinkk',
-        mainimage:'mainimage',
-        images:['image2','image3'],
-    }
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:2,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },   {
+        Brand:'OTTO',
+        Title:'WEIMAR-40mm',
+        Rate:3,
+        Price:'2.299.000 đ',
+        About:'Thiết kế Bauhaus tối giản của Weimar Otto được Curnon thể hiện trên nền của sự kết hợp màu sắc tinh tế và hiện đại. Cặp dây da màu Sand cùng chi tiết kim xanh chính là điểm nhấn giúp bạn kết hợp hoàn hảo với bất kì bộ trang phục nào.',
+        firstlink:'firstlinkk',
+        secondlink:'secondlinkk',
+        mainimage:'image 6.png',
+        images:['image 3.png','image 4.png','image 5.png'],
+    },
 ]
 
-console.log(watches)
+const generallogo='Logo.png';
+
+const products=document.querySelector("#products")
+watches.forEach((card)=>{
+   products.innerHTML+=`
+   <div class="watch">
+   <div class="generalicon">
+    <img src="./assets/icons/${generallogo}" alt="">
+   </div>
+   <div class="content">
+    <div class="sideimages">
+       <img src="./assets/images/${card.images[0]}" alt="">
+       <img src="./assets/images/${card.images[1]}" alt="">
+       <img src="./assets/images/${card.images[2]}" alt="">
+    </div>
+    <div class="mainimage">
+       <img src="./assets/images/${card.mainimage}" alt="">
+    </div>
+    <div class="info">
+         <div class="brand">${card.Brand}</div>
+         <div class="Title">${card.Title}</div>
+         <div class="rating">
+         ${
+            `<i style="color: red; width: 17px;" class="fa-sharp fa-solid fa-star"></i>`.repeat(card.Rate)+
+            '<img src="./assets/images/iwwa_star.png" alt="">'.repeat(5 - card.Rate)
+         }
+         
+         </div>
+         <div class="price">
+               <p>PRICE:</p>
+               <p>${card.Price}</p>
+         </div>
+         <div class="about">
+         ${card.About}
+         </div>
+         <a src="${card.firstlink}" class="firstBtn">
+            <img src="./assets/images/Frame 8.png" alt="">
+         </a>
+         <a src="${card.secondlink}" class="secBtn">MUA NGAY</a>
+    </div>
+   </div>
+</div>
+   `
+})
